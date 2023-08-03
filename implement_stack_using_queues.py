f841e20e32f4a13400ce24dfc5f27531aac63793
yarynka28 <104198926+yarynka28@users.noreply.github.com>
2023-08-03 12:59:15 +0300

    Update implement_stack_using_queues.py

--- implement_stack_using_queues.py
+++ implement_stack_using_queues.py
@@ -1,19 +1,29 @@
+import queue
+
 class MyStack:
 
     def __init__(self):
-        self.queue = []
+        self.q = queue.Queue()
 
     def push(self, x: int) -> None:
-        self.queue.append(x)
+        self.q.put(x)
 
     def pop(self) -> int:
-        return self.queue.pop()
+        temp_q = queue.Queue()
+        while self.q.qsize() > 1:
+            temp_q.put(self.q.get())
+        top_element = self.q.get()
+        while not temp_q.empty():
+            self.q.put(temp_q.get())
+        return top_element
 
     def top(self) -> int:
-        return self.queue[-1]
+        top_element = self.pop()
+        self.q.put(top_element)
+        return top_element
 
     def empty(self) -> bool:
-        return not self.queue 
+        return self.q.empty()
 
 
 # Your MyStack object will be instantiated and called as such:
